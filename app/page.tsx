fbec731499c6cd33fad358d404fe46cde59e0ffb
soso-droid22 <soso.vardoshvili22@gmail.com>
2024-07-22 00:19:43 +0400

    gg

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import Main from './components/Main'
+import React, { useEffect } from 'react';
+import Main from './components/Main';
 
 export default function Home() {
+  useEffect(() => {
+    // Check if service workers are supported by the browser
+    if ('serviceWorker' in navigator) {
+      // Register the service worker
+      navigator.serviceWorker.register('/sw.js')
+        .then((registration) => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch((error) => {
+          console.log('ServiceWorker registration failed: ', error);
+        });
+    }
+  }, []);
+
   return (
-    <div className=' h-screen  overflow-y-auto'>
+    <div className='h-screen overflow-y-auto'>
       <Main />
     </div>
-  )
+  );
 }
