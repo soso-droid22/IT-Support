b9f1ce6e8f613983692b2639ffd99036fb2e9253
soso-droid22 <soso.vardoshvili22@gmail.com>
2024-07-21 22:42:40 +0400

    gg

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +22,8 @@ export default function RootLayout({
        <body className={inter.className}>
        <meta charSet="UTF-8" />
        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <SpeedInsights/>
+        <Analytics/>
          <Header />
        <main>{children}</main>
       </body>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+export default {
+  output: 'export',
+  // other configurations
+};
